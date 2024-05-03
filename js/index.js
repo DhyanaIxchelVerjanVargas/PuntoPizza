f89aa4f3008eebc3b148154796b438e18eb3ee27
dhyana <dhyana.ixchel@gmail.com>
2024-05-03 14:43:14 -0600

    Se corrigió el menú responsivo añadiendo una funcionalidad en el archivo javascript y se le añadió el scroll-behavior:smooth en el css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
+cargarEventListeners();
+function cargarEventListeners(){
+    //Quitar el menú
+    navMenu.addEventListener('click', removeClass);
+}
+
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 })
+
+function removeClass(e){
+    if(e.target.classList.contains('nav-menu-link')){
+        navMenu.classList.remove("nav-menu_visible");
+    }
+}
